9e166965d8b66950da468f88685b7a22705232f3
Nasrul Hazim <nasrulhazim.m@gmail.com>
2019-10-13 10:19:06 +0800

    Added make:schema command

---
+++ app/Commands/Api/MakeSchemaCommand.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Commands\Api;
+
+use Illuminate\Console\Command;
+
+class MakeSchemaCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'make:schema
+                            {table : Targeted table}
+                            {version : OAS Version}
+                            {--connection=sqlite : Database connection}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Create a new OAS Schema';
+
+    /**
+     * Create a new controller creator command instance.
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $connection = $this->option('connection');
+        $table      = $this->argument('table');
+        $version    = $this->argument('version');
+
+        \App\Processors\ApiSchema::make($version)
+            ->setConnection($connection)
+            ->setTable($table)
+            ->generate();
+
+        $this->info('Successfully created OpenAPI Specification schema version ' . $version . ' for table ' . $table . '.');
+    }
+}
